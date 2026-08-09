681123e314f958a06781025b9f768380a4e53b8e
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-08-09 22:21:43 +0530

    model downloaded

---
+++ utils/model_loader.py
@@ -0,0 +1,4 @@
+from joblib import load
+from utils.path_utils import MODEL_PATH
+
+def load_model(model_name='')
---
+++ utils/path_utils.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_ROOT=Path(__file__).resolve().parent[1]
+
+MODEL_PATH=PROJECT_ROOT /"models"
+CONFIG_PATH=PROJECT_ROOT/"config"
