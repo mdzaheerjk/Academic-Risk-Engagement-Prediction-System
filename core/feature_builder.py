a432a75ea6864fab28d924647b660206203abd54
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-08-09 22:09:18 +0530

    feature builder

---
+++ core/feature_builder.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def build_features(student_dict):
+    df=pd.DataFrame([student_dict])
+
+    features=["total_click",
+              'early_click',
+              'early_active_days',
+              'first_active_day',
+              'pre_course_engaged'
+              ]
+    df['first_activity_day']=df['first_activity_day'].fillna(999)
+
+    df=df[features].fillna(0)
+
+    return df
